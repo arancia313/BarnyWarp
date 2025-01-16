718fc54a142468c6a6837343badb39917f466749
arancia313 <marraalan89@gmail.com>
2025-01-16 18:59:40 +0000

    A bunch of upgraded stuff.

---
+++ github/workflows/Style.svelte
@@ -0,0 +1,43 @@
+
+
+<style>
+   body {
+       font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+       margin: 0;
+       padding: 0;
+       height: 100vh;
+       display: flex;
+       align-items: center;
+       justify-content: center;
+       background-color: rgb(0, 0, 0);
+   }
+   h1{
+       color: orangered; /* OrangeRed color for better contrast */
+       font-size: 3em;
+       text-align: center;
+       text-decoration: none;
+   }
+   p {
+       color: white; /* White color for better contrast */
+       font-size: 1.5em;
+       text-align: center;
+       text-decoration: none;
+   }
+   a {
+       color: aliceblue; /* AliceBlue color for links */
+      font-size: 1.5em;
+      text-align: center;
+      text-decoration: underline; 
+   }
+   button {
+      background-color: Orange;
+      margin: 0;
+      padding: 0;
+      color: white; /* White color for better contrast */
+      border-radius: 5px;
+      font-size: 14px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+   }
+</style>
--- index.js
+++
@@ -1,4 +0,0 @@
-alert('Hello!');
-if (confirm('Still continue?') === true) {
-    // codice da eseguire se l'utente preme "OK"
-    }
