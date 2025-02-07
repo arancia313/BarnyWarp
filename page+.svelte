3b9b7918be9d7c0a2ed9ad2152eee6e74af2c417
arancia313 <marraalan89@gmail.com>
2025-02-07 14:45:20 +0000

    A bunch of stuff.

---
+++ page+.svelte
@@ -0,0 +1,20 @@
+<script>
+              console.log('Little Code.')
+              import { onMount } from 'svelte';
+              import { page } from '$app/stores';
+              import { goto } from '$app/navigation';
+              
+              let path = '';
+              
+              onMount(() => {
+              const unsubscribe = page.subscribe(value => {
+              path = value.path;
+              });
+              
+              return unsubscribe;
+              });
+              
+              function navigate() {
+              goto(path);
+              }
+</script>
