bcb7be9dccb3bfe2b499fc10f8be940320acbf78
Alan Marra <marraalan89@gmail.com>
2025-02-27 17:00:17 +0000

    A bunch of stuff.

--- troppen.config.ts
+++ troppen.config.ts
@@ -1,16 +1,17 @@
+//A troppenconfig.ts file is a configuration for the Troppen.js File.
 type TroppenConfig = {
-              [key: string]: any;
+  [key: string]: any;
 };
 
 const Function = (config: TroppenConfig) => {
-              const get = (key: string) => config[key];
-              const set = (key: string, value: any) => {
-                            config[key] = value;
-                            return config;
-              };
-              return {
-                            config,
-                            get,
-                            set
-              };
-}
+  const get = (key: string) => config[key];
+  const set = (key: string, value: any) => {
+    config[key] = value;
+    return config;
+  };
+  return {
+    config,
+    get,
+    set,
+  };
+};
