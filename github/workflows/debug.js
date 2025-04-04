87d3665c06550157a989a5c9554d6bc2a500a5c6
arancia313 <marraalan89@gmail.com>
2025-04-04 19:40:49 +0000

    LITERALLY.

--- github/workflows/debug.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('rar').onclick = function() {
-              console.log('False statement.',
-              "if you don't know what this window does, EXIT NOW!!!",
-              "People can use it for uploading inappropiate projects under your NAME, delete yor projects, and get you banned from BarnyWarp!",
-              )
-}
