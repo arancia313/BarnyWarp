89b1e48865d143ac01e2b34a9394a3916c02fb1b
arancia313 <marraalan89@gmail.com>
2025-10-08 23:36:07 +0200

    Those things seems to be USELESS.

--- static/github/workflows/configs./config.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-              if (Scratch.extension.unsandboxed) {
-                            console.log('I must execute this!')
-                            (function(Scratch) {
-                                          const extension = new extensionBuilder(); 
-                            })(Window.Scratch)  
-              }
-</script>
-                                 
