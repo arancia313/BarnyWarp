b514a58111477f70203a722859175fe3005f2dca
arancia313 <marraalan89@gmail.com>
2025-11-02 21:18:19 +0000

    If i ever dare to check the console and see that log, It means the server is ready, i think...

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import { createServer } from 'node:http';
 const server = createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World!\n');
+  console.log("Server just responded to my Handshake!")
 });
 
 // starts a simple http server locally on port 3000
